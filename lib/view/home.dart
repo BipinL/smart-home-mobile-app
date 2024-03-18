dba82114fbac8f4d5ae47ed954a5420bd9848e25
BipinL <104549520+BipinL@users.noreply.github.com>
2024-03-18 16:33:02 +0545

    row one of first screen finished

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -11,6 +12,8 @@ class HomeView extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -25,8 +28,8 @@ class HomeView extends StatelessWidget {
                     width: 90,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
-                        color: Color(0xff353535)),
-                    child: Row(
+                        color: const Color(0xff353535)),
+                    child: const Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
                           Text(
@@ -41,6 +44,164 @@ class HomeView extends StatelessWidget {
                   ),
                 ],
               ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Good Day. Rima!",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                "Manage your home",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        height: 230,
+                        width: 160,
+                        decoration: BoxDecoration(
+                            color: Color(0xff343434),
+                            borderRadius: BorderRadius.circular(30)),
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'images/lamp.png',
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            const Text(
+                              "Smart Lightinig",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            const Text(
+                              "100% Led Light",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xff565454)),
+                            )
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        top: 200,
+                        left: 45,
+                        child: Container(
+                          height: 40,
+                          width: 70,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white),
+                        ),
+                      ),
+                      Positioned(
+                        top: 205,
+                        left: 50,
+                        child: Container(
+                          height: 25,
+                          width: 60,
+                          decoration: BoxDecoration(
+                              color: const Color(0xffe9ecf4),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Row(children: [
+                            Image.asset('images/switch_button.png'),
+                          ]),
+                        ),
+                      )
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        height: 230,
+                        width: 160,
+                        decoration: BoxDecoration(
+                            color: const Color(0xffbcffcb),
+                            borderRadius: BorderRadius.circular(30)),
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'images/speaker.png',
+                              height: 100,
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            const Text(
+                              "Portable Speaker",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        top: 160,
+                        left: 5,
+                        child: Container(
+                          height: 60,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: Colors.white),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Image.asset(
+                                  'images/playbutton.png',
+                                  scale: 5,
+                                ),
+                                const Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Text(
+                                      "Loading Game",
+                                      style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      "Fun Unlimited",
+                                      style: TextStyle(
+                                        fontSize: 13,
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
             ],
           ),
         ),
